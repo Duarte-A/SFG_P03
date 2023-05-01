5033c7c7a1617a2c3c978ee5f86e5e5f37c68a14
Duarte-A <87626352+Duarte-A@users.noreply.github.com>
2023-05-01 15:08:18 -0500

    Building mesh change by health
    The building in the level will change mesh depending on it's health integer value. The building's health is checked in a function which is placed in a fixed update. For testing, the user can decrease the building's health value by one integer when they press 'A'
    
    If it is at a value of 3 or higher then it is "undamaged"
    
    If it is at a value between 1 and 2 then it will change into a "damaged" mesh, and play a simple placeholder smoke particle, which can be replaced by another particle object.
    
    If it is at a value of 0 or less than 0 then it will change into a "ruined" mesh, and play a simple placeholder explosion and fire particle, which can be replaced by another particle object.

--- Assets/Script/Tool/BuildingDestruction.cs
+++ Assets/Script/Tool/BuildingDestruction.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class BuildingDestruction : MonoBehaviour
 {
-    [Header("Building Stats")]
-    //add building health
+    [Header("Building State")]
+    //add building health (use ints)
+    //[SerializeField] private BuildingType _buildingType = BuildingType.None;
+    public int _buildingHealth;
+
 
     [Header("Undamaged")]
     [SerializeField] GameObject _undamaged;
@@ -14,68 +19,251 @@ public class BuildingDestruction : MonoBehaviour
     [Header("Damaged")]
     [SerializeField] GameObject _damaged;
     public bool _isDamaged;
-    //add smoke particle resulted from damage
+    public GameObject _smoke;
+    public bool _canExplodeOnTimer;
     //add optional timer to explode into ruins
 
     [Header("Ruins")]
     [SerializeField] GameObject _ruins;
-    public bool _isRuined;
-    //add explosion particle
+    public bool _isRuined = false;
+    public GameObject _explosion;
     //add explosion damage and damage radius
-    //add fire particle resulted from explosion
+    public GameObject _fire;
 
+    private float fixedUpdateCount = 0;
+
+    public enum BuildingType
+    {
+        None = 0,
+        Undamaged,
+        Damaged,
+        Ruined
+    }
 
     private void Update()
     {
-        SetUndamaged();
-        SetDamaged();
-        SetRuined();
-    }
-    public void SetUndamaged()
-    {
-        if(_isUndamaged == true)
+        /*
+            //set building states according to health
+
+            SetUndamagedCheck();
+            SetDamagedCheck();
+            SetRuinedCheck();
+        */
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-        bool damagedBool = _isDamaged == false;
-        bool ruinsBool = _isRuined == false;
-        _undamaged.SetActive(true);
-        _damaged.SetActive(false);
-        _ruins.SetActive(false);
+            TakeDamage(1);
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        fixedUpdateCount += 1;
+        /*
+                if (_isUndamaged && !_isDamaged && !_isRuined)
+                {
+                    _isUndamaged = true;
+                    _isDamaged = false;
+                    _isRuined = false;
+                    SetUndamagedCheck();
+                    Debug.Log("Undamaged");
+                }
+
+                if (_isDamaged && !_isUndamaged && !_isRuined)
+                {
+                    _isUndamaged = false;
+                    _isDamaged = true;
+                    _isRuined = false;
+                    SetDamagedCheck();
+                    Debug.Log("Damaged");
+                }
+
+                if (_isRuined && !_isUndamaged && _isRuined)
+                {
+                    _isUndamaged = false;
+                    _isDamaged = false;
+                    _isRuined = true;
+                    SetRuinedCheck();
+                    Debug.Log("Ruins");
+                }
+        */
+
+        HealthCheck();
+    }
+
+    //Single Bool Check Function Below
+
+    public void HealthCheck()
+    {
+        if (_buildingHealth >= 3 && _isUndamaged)
+        {
+
+            Debug.Log("Undamaged");
+            _undamaged.SetActive(true);
+            _damaged.SetActive(false);
+            _ruins.SetActive(false);
+
+            bool damagedBool = _isDamaged == false;
+            bool ruinsBool = _isRuined == false;
         }
         else
         {
-        _undamaged.SetActive(false);
+            _undamaged.SetActive(false);
+        if (_buildingHealth <= 2 ||  _buildingHealth >= 1)
+        {
+            _isUndamaged = false;
+            _isDamaged = true;
+            _isRuined = false;
+            Debug.Log("Damaged");
+            if (_isDamaged == true)
+            {
+                bool undamagedBool = _isDamaged == false;
+                bool ruinsBool = _isRuined == false;
+                _undamaged.SetActive(false);
+                _damaged.SetActive(true);
+                _ruins.SetActive(false);
+            }
+            else
+            {
+                _damaged.SetActive(false);
+            }
+        }
+
+        if (_buildingHealth <= 0)
+        {
+            _isUndamaged = false;
+            _isDamaged = false;
+            _isRuined = true;
+            Debug.Log("Ruined");
+            if (_isRuined == true)
+            {
+                bool undamagedBool = _isDamaged == false;
+                bool damagedBool = _isRuined == false;
+                _undamaged.SetActive(false);
+                _damaged.SetActive(false);
+                _ruins.SetActive(true);
+            }
+            else
+            {
+                _ruins.SetActive(false);
+                    Debug.Log("Else Ruined");
+                }
+        }
+        }
+
+    }
+
+    //Separate Bool functions below
+
+    public void SetUndamagedCheck()
+    {
+        if (_buildingHealth >= 3)
+        {
+            if (_isUndamaged == true)
+            {
+                bool damagedBool = _isDamaged == false;
+                bool ruinsBool = _isRuined == false;
+                _undamaged.SetActive(true);
+                _damaged.SetActive(false);
+                _ruins.SetActive(false);
+            }
+            else
+            {
+                _undamaged.SetActive(false);
+            }
         }
     }
 
-    public void SetDamaged()
+    public void SetDamagedCheck()
     {
-        if (_isDamaged == true)
+        if (_buildingHealth >= 1 || _buildingHealth <= 2)
         {
-            bool undamagedBool = _isDamaged == false;
-            bool ruinsBool = _isRuined == false;
+            if (_isDamaged == true)
+            {
+                bool undamagedBool = _isDamaged == false;
+                bool ruinsBool = _isRuined == false;
+                _undamaged.SetActive(false);
+                _damaged.SetActive(true);
+                _ruins.SetActive(false);
+            }
+            else
+            {
+                _damaged.SetActive(false);
+            }
+        }
+    }
+
+    public void SetRuinedCheck()
+    {
+        if (_buildingHealth <= 0)
+        {
+            if (_isRuined == true)
+            {
+                bool undamagedBool = _isDamaged == false;
+                bool damagedBool = _isRuined == false;
+                _undamaged.SetActive(false);
+                _damaged.SetActive(false);
+                _ruins.SetActive(true);
+            }
+            else
+            {
+                _ruins.SetActive(false);
+            }
+        }
+    }
+
+    //Health
+
+    void TakeDamage(int damage)
+    {
+        _buildingHealth -= damage;
+    }
+
+    /*
+    //Enum functions below
+
+    public void UndamagedType()
+    {
+        if(_buildingType == BuildingType.Undamaged)
+        {
+            _undamaged.SetActive(true);
+            _damaged.SetActive(false);
+            _ruins.SetActive(false);
+        }
+        else
+        {
+            _undamaged.SetActive(false);
+        }
+    }
+
+    public void DamagedType()
+    {
+        if (_buildingType == BuildingType.Damaged)
+        {
             _undamaged.SetActive(false);
             _damaged.SetActive(true);
             _ruins.SetActive(false);
         }
         else
         {
-        _damaged.SetActive(false);
+            _damaged.SetActive(false);
         }
     }
 
-    public void SetRuined()
+    public void RuinedType()
     {
-        if (_isRuined == true)
+        if (_buildingType == BuildingType.Ruined)
         {
-            bool undamagedBool = _isDamaged == false;
-            bool damagedBool = _isRuined == false;
             _undamaged.SetActive(false);
             _damaged.SetActive(false);
             _ruins.SetActive(true);
         }
         else
         {
-        _ruins.SetActive(false);
+            _ruins.SetActive(false);
         }
     }
+    */
 }
